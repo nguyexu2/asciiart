562af5198dc16f5963ee37bf884d13d0793d236f
Xuan Thang Nguyen <nguyexu2@fit.cvut.cz>
2020-12-20 22:19:18 +0100

    rgb pixel val to def for color values

--- src/main/scala/Image/Pixel.scala
+++ src/main/scala/Image/Pixel.scala
@@ -4,10 +4,10 @@ import java.awt.Color
 
 abstract class Pixel {}
 
-class RGBPixel(val color:Color) extends Pixel {
-  val red = color.getRed()
-  val green = color.getGreen()
-  val blue = color.getBlue()
+class RGBPixel(val color: Color) extends Pixel {
+  def red: Int = color.getRed
+  def green: Int = color.getGreen
+  def blue: Int = color.getBlue
 }
 
-class CharPixel(val char: Char) extends Pixel  {}
+class CharPixel(val char: Char) extends Pixel {}
