aab6a9535dbb0c607cffe04f0734ce7fa55e6f98
Xuan Thang Nguyen <nguyexu2@fit.cvut.cz>
2020-12-21 00:07:59 +0100

    rotation and tests

--- src/main/scala/ImageFilter/RotateFilter.scala
+++ src/main/scala/ImageFilter/RotateFilter.scala
@@ -1,6 +1,34 @@
 package ImageFilter
-import Image.Image
+
+import scala.reflect.ClassTag
 
 class RotateFilter(val degree: Int) extends Filter {
-  override def filter(arr:Array[Array[_]]) :Array[Array[_]] = ???
+  final val supportedDegree = Array[Int](0, 90, 180, 270)
+
+  private final val normalizedDegree =
+    if (degree < 0) {
+      degree + 360 * ((degree.abs / 360) + 1)
+    }
+    else degree % 360
+
+  if (!supportedDegree.contains(normalizedDegree))
+    throw new IllegalArgumentException(s"rotation by $degree not supported")
+
+  override def filter[T: ClassTag](arr: Array[Array[T]]): Array[Array[T]] = {
+    var ret = arr.map(_.clone())
+    var tmpDeg = normalizedDegree
+    while (tmpDeg != 0) {
+      val height = ret.length
+      val width = ret(0).length
+      val tmpMat = Array.ofDim[T](width, height)
+      for (i <- 0 until height; j <- 0 until width)
+        tmpMat(j)(height - 1 - i) = ret(i)(j)
+
+      ret = tmpMat
+      tmpDeg = tmpDeg - 90
+    }
+
+
+    ret
+  }
 }
---
+++ src/test/scala/RotateTest.scala
@@ -0,0 +1,59 @@
+import ImageFilter.RotateFilter
+import org.scalatest.FunSuite
+
+class RotateTest extends FunSuite {
+  test("0 rotation") {
+    val rotator = new RotateFilter(0)
+    val input = Array(Array(1, 2, 3), Array(4, 5, 6))
+    val res = rotator.filter(input)
+    assert(res.length == 2)
+    assert(res(0).sameElements(Array(1, 2, 3)))
+    assert(res(1).sameElements(Array(4, 5, 6)))
+  }
+
+  test("+90 rotation") {
+    val rotator = new RotateFilter(90)
+    val input = Array(Array(1, 2, 3), Array(4, 5, 6))
+    val res = rotator.filter(input)
+    assert(res.length == 3)
+    assert(res(0).sameElements(Array(4, 1)))
+    assert(res(1).sameElements(Array(5, 2)))
+    assert(res(2).sameElements(Array(6, 3)))
+  }
+
+  test("+180 rotation") {
+    val rotator = new RotateFilter(180)
+    val input = Array(Array(1, 2, 3), Array(4, 5, 6))
+    val res = rotator.filter(input)
+    assert(res.length == 2)
+    assert(res(0).sameElements(Array(6, 5, 4)))
+    assert(res(1).sameElements(Array(3, 2, 1)))
+  }
+
+  test("-270 rotation") {
+    val rotator = new RotateFilter(-270)
+    val input = Array(Array(1, 2, 3), Array(4, 5, 6))
+    val res = rotator.filter(input)
+    assert(res.length == 3)
+    assert(res(0).sameElements(Array(4, 1)))
+    assert(res(1).sameElements(Array(5, 2)))
+    assert(res(2).sameElements(Array(6, 3)))
+  }
+
+  test("over 720 rotation"){
+    val rotator = new RotateFilter(720)
+    val input = Array(Array(1, 2, 3), Array(4, 5, 6))
+    val res = rotator.filter(input)
+    assert(res.length == 2)
+    assert(res(0).sameElements(Array(1, 2, 3)))
+    assert(res(1).sameElements(Array(4, 5, 6)))
+  }
+
+  test("not supported angle"){
+    assertThrows[IllegalArgumentException](new RotateFilter(1))
+  }
+
+  test("not supported angle 2"){
+    assertThrows[IllegalArgumentException](new RotateFilter(-456184))
+  }
+}
