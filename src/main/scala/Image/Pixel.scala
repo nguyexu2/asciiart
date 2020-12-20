5fd2df8b0d4967fc5deb0e3635964eb5f16090ad
Xuan Thang Nguyen <nguyexu2@fit.cvut.cz>
2020-12-20 01:52:29 +0100

    rgb for RGBpixel

--- src/main/scala/Image/Pixel.scala
+++ src/main/scala/Image/Pixel.scala
@@ -4,6 +4,10 @@ import java.awt.Color
 
 abstract class Pixel {}
 
-class RGBPixel(val color:Color) extends Pixel {}
+class RGBPixel(val color:Color) extends Pixel {
+  val red = color.getRed()
+  val green = color.getGreen()
+  val blue = color.getBlue()
+}
 
 class CharPixel(val char: Char) extends Pixel  {}
--- src/main/scala/ImageConversion/ConvertRGBtoChar.scala
+++ src/main/scala/ImageConversion/ConvertRGBtoChar.scala
@@ -6,6 +6,6 @@ class ConvertRGBtoChar extends Convert {
   private final val grayScaleTable = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
 
   private def ConvertToChar(pixel: RGBPixel):CharPixel = {
-
+    val gray = (0.3 * Red) + (0.59 * Green) + (0.11 * Blue))
   }
 }
