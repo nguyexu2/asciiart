1e3b4618b7582a93671741f42b9da2d6ab911bda
Xuan Thang Nguyen <nguyexu2@fit.cvut.cz>
2020-12-20 02:19:28 +0100

    refactor conversionTable

--- src/main/scala/ImageConversion/ConvertRGBtoChar.scala
+++ src/main/scala/ImageConversion/ConvertRGBtoChar.scala
@@ -1,14 +1,14 @@
 package ImageConversion
 
 import Image.{CharPixel, RGBPixel}
+import ImageConversion.Table.{ConversionTable, grayscaleTable}
 
-class ConvertRGBtoChar extends Convert {
-  private final val grayScaleTable = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+class ConvertRGBtoChar(val grayScaleTable:ConversionTable = new grayscaleTable) extends Convert {
 
   def ConvertToChar(pixel: RGBPixel):CharPixel = {
     val gray = (0.3 * pixel.red) + (0.59 * pixel.green) + (0.11 * pixel.blue)
     val index:Int = (gray / grayScaleTable.length).toInt
-    val charRepresentation = grayScaleTable(index)
+    val charRepresentation = grayScaleTable.getChar(index)
     return new CharPixel(charRepresentation)
   }
 }
