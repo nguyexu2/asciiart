a2d7517bd4dcaad89a5fe0f6e491eb879172d6ff
Xuan Thang Nguyen <nguyexu2@fit.cvut.cz>
2020-12-20 22:22:48 +0100

    refactor image to unify

--- src/main/scala/Image/Image.scala
+++ src/main/scala/Image/Image.scala
@@ -1,4 +1,15 @@
 package Image
 
-abstract class Image {
+import scala.reflect.ClassTag
+
+class Image[T <: Pixel:ClassTag](pixelGrid: PixelGrid[T]) {
+  def this(array: Array[Array[T]]) = this(new PixelGrid[T](array))
+
+  val width :Int= pixelGrid.width
+  val height:Int = pixelGrid.height
+
+  def getPixel(x: Int, y: Int):T = pixelGrid.getPixel(x, y)
+
+  def getGrid:Array[Array[T]] = pixelGrid.getGrid
 }
+
