8e1b0478984f6ec5754336d339d0e231a741d43e
Xuan Thang Nguyen <nguyexu2@fit.cvut.cz>
2020-12-21 01:42:12 +0100

    brightness and tests

--- src/main/scala/ImageFilter/ChangeBrightnessFilter.scala
+++ src/main/scala/ImageFilter/ChangeBrightnessFilter.scala
@@ -2,6 +2,14 @@ package ImageFilter
 
 import scala.reflect.ClassTag
 
-class ChangeBrightnessFilter(offset :Int) extends Filter {
-  override def filter[T:ClassTag](arr:Array[Array[T]]) :Array[Array[T]] = ???
+class ChangeBrightnessFilter[A](f: A => A) extends Filter {
+
+  override def filter[T: ClassTag](arr: Array[Array[T]]): Array[Array[T]] = {
+    if (!arr(0)(0).isInstanceOf[A])
+      throw new IllegalArgumentException("type do not match")
+
+    arr.map(_.map(x => {
+      f(x.asInstanceOf[A]).asInstanceOf[T]
+    }))
+  }
 }
---
+++ src/test/scala/Filters/BrightnessChangeTester.scala
@@ -0,0 +1,22 @@
+package Filters
+
+import ImageFilter.ChangeBrightnessFilter
+import org.scalatest.FunSuite
+
+class BrightnessChangeTester extends FunSuite {
+  test("no brightness change") {
+    val filter = new ChangeBrightnessFilter( (e:Int) => e*50)
+    val input = Array(Array(0, 1), Array(5, -1))
+    val output = filter.filter(input)
+    assert(output.length == input.length)
+    assert(output(0).sameElements(Array(0, 50)))
+    assert(output(1).sameElements(Array(250, -50)))
+  }
+
+  test("type mismatch") {
+    val filter = new ChangeBrightnessFilter( (e:Int) => e*50)
+    val input = Array(Array("a", "b"))
+    assertThrows[Throwable](filter.filter(input))
+  }
+
+}
