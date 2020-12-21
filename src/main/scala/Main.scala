9b434fd41ade123231bb6c02750a22d1c609b81f
Xuan Thang Nguyen <nguyexu2@fit.cvut.cz>
2020-12-21 13:21:36 +0100

    finilize main app

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,20 +3,29 @@ import ImageConversion.ConverterFactory
 import ImageFilter.ArrayFilters.RotateFilter
 import ImageFilter.FilterFactory
 import Loader.LoaderFactory
-import Saver.ConsolePrint
+import Saver.{ConsolePrint, SaverFactory}
 import UI.InputParser
 
 object Main extends App {
   val paramList = new InputParser(args).Parse()
 
   val loader = LoaderFactory.get(paramList)
-  val image = loader.getImage
+  if (loader.isEmpty) {
+    println("couldn't fetch image, input uses --image file_path")
+    sys.exit(1)
+  }
+
+  val saverFunc = SaverFactory.get(paramList)
+  if (saverFunc.isEmpty) {
+    println("couldn't fetch output param, try to use --output-console")
+    sys.exit(1)
+  }
+
+  val image = loader.get.getImage
   val newImage = ConverterFactory.RGBImageToCharImage(image)
 
   val filter = FilterFactory.get(paramList)
 
   val newFilteredImage = new Image(filter.filter(newImage.getGrid))
-
-  //save image
-  ConsolePrint.printToConsole(newFilteredImage)
+  saverFunc.get(newFilteredImage)
 }
