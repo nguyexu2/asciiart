2e6458911507f96893414bd0c49411876ac6cbc3
Xuan Thang Nguyen <nguyexu2@fit.cvut.cz>
2020-12-21 21:53:06 +0100

    inverter test and comments

--- src/test/scala/ImageFilter/ArrayFilters/InvertFilterTest.scala
+++ src/test/scala/ImageFilter/ArrayFilters/InvertFilterTest.scala
@@ -3,12 +3,12 @@ package ImageFilter.ArrayFilters
 import org.scalatest.FunSuite
 
 class InvertFilterTest extends FunSuite{
+
   test("normal change") {
-    val filter = new InvertFilter( (e:Int) => if(e >= 255) 0 else 255 -e)
+    val filter = new InvertFilter( (e:Int) => 255 -e)
     val input = Array(Array(255, 50), Array(60, 0))
     val output = filter.filter(input)
 
-    input(0)(0) = -454054
     assert(output.length == input.length)
     assert(output(0).sameElements(Array(0, 255 -50)))
     assert(output(1).sameElements(Array(255-60, 255)))
@@ -18,5 +18,24 @@ class InvertFilterTest extends FunSuite{
     val filter = new InvertFilter( (e:Int) => e*50)
     val input = Array(Array("a", "b"))
     assertThrows[Throwable](filter.filter(input))
+  }
+
+  test("empty input"){
+    val filter = new InvertFilter( (e:Int) => 255 -e)
+    val input = Array(Array[Int]())
+    val output = filter.filter(input)
+    assert(output.length == 1 && output(0).length == 0)
+  }
+
+  test("immutability"){
+    val filter = new InvertFilter( (e:Int) => 255 -e)
+    val input = Array(Array(255, 50), Array(60, 0), Array(200, 100))
+    val output = filter.filter(input)
+
+    input(0)(0) = -454054
+    assert(output.length == input.length)
+    assert(output(0).sameElements(Array(0, 255 -50)))
+    assert(output(1).sameElements(Array(255-60, 255)))
+    assert(output(2).sameElements(Array(255-200, 255-100)))
   }
 }
