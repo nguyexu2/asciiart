1d715d7aba21c538d5c5ea657b2b321fb47478db
Xuan Thang Nguyen <nguyexu2@fit.cvut.cz>
2020-12-21 19:46:38 +0100

    checking size

--- src/main/scala/ImageFilter/ArrayFilters/ScaleFilter.scala
+++ src/main/scala/ImageFilter/ArrayFilters/ScaleFilter.scala
@@ -4,12 +4,19 @@ import scala.reflect.ClassTag
 
 abstract class ScaleFilter extends Filter {}
 
-class ScaleUpFilter[A](value: Double) extends ScaleFilter{
+class ScaleUpFilter(value: Double) extends ScaleFilter{
   if(value < 1)
     throw new IllegalArgumentException(s"scaling up needs a value >=1, got $value")
 
   override def filter[T: ClassTag](arr: Array[Array[T]]): Array[Array[T]] = {
-    val newHeight = (arr.length * value).toInt
+    if(arr.length == 0 || arr(0).isEmpty)
+      return arr
+
+    val width = arr.length
+    if(arr.exists(row => row.length != width))
+      throw new IllegalArgumentException("not all rows have the same length")
+
+    val newHeight = (width * value).toInt
     val newWidth = (arr(0).length * value).toInt
     val ret = Array.ofDim[T](newHeight, newWidth)
 
@@ -25,7 +32,14 @@ class ScaleDownFilter[A](value: Double, average: Seq[A] => A) extends ScaleFilte
     throw new IllegalArgumentException(s"scaling up needs a value (0 - 1] , got $value")
 
   override def filter[T: ClassTag](arr: Array[Array[T]]): Array[Array[T]] = {
-    val newHeight = (arr.length * value).toInt
+    if(arr.length == 0 || arr(0).isEmpty)
+      return arr
+
+    val width = arr.length
+    if(arr.exists(row => row.length != width))
+      throw new IllegalArgumentException("not all rows have the same length")
+
+    val newHeight = (width * value).toInt
     val newWidth = (arr(0).length * value).toInt
     val ret = Array.ofDim[T](newHeight, newWidth)
 
@@ -33,10 +47,9 @@ class ScaleDownFilter[A](value: Double, average: Seq[A] => A) extends ScaleFilte
     val w = arr(0).length / newWidth
 
     for (i <- 0 until newHeight; j <- 0 until newWidth) {
-      val list :Seq[A] = for(ii <- i*h until (i+1)*h; jj <- j*h until (j+1)*h) yield arr(ii)(jj).asInstanceOf[A]
+      val list :Seq[A] = for(ii <- i*h until (i+1)*h; jj <- j*w until (j+1)*w) yield arr(ii)(jj).asInstanceOf[A]
 
       ret(i)(j) = average(list).asInstanceOf[T]
-
     }
     ret
   }
