f162f2de82d19e32caa80fe265c14a7da59ae644
Xuan Thang Nguyen <nguyexu2@fit.cvut.cz>
2020-12-21 21:49:10 +0100

    comments

--- src/main/scala/ImageFilter/ArrayFilters/FlipFilter.scala
+++ src/main/scala/ImageFilter/ArrayFilters/FlipFilter.scala
@@ -5,11 +5,19 @@ import scala.reflect.ClassTag
 abstract class FlipFilter extends Filter {}
 
 class FlipXFilter extends FlipFilter {
+  /**
+   * reverses array on the X axis
+   * for each row: last element becomes 1st and the other way round
+   */
   override def filter[T:ClassTag](arr: Array[Array[T]]): Array[Array[T]] =
     arr.map(_.reverse)
 }
 
 class FlipYFilter extends FlipFilter {
+  /**
+   * reverses array on the Y axis
+   * 1st row will become last row and the other way round
+   */
   override def filter[T:ClassTag](arr: Array[Array[T]]): Array[Array[T]] =
     arr.map(_.clone()).reverse
 }
