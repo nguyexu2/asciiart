2d246f86b483a63badafce53178a32a19851d78c
Xuan Thang Nguyen <nguyexu2@fit.cvut.cz>
2020-12-21 13:54:45 +0100

    inverter creation in factory

--- src/main/scala/ImageConversion/ConverterFactory.scala
+++ src/main/scala/ImageConversion/ConverterFactory.scala
@@ -1,16 +1,22 @@
 package ImageConversion
 
 import Image.{CharPixel, Image, RGBPixel}
-import ImageConversion.Table.ConvertRGBtoChar
+import ImageConversion.Table.{BourkeTable, ConvertRGBtoChar}
+import ImageConversion.Table.PixelUtil.Inverter
 
 object ConverterFactory {
   def RGBImageToCharImage: Image[RGBPixel] => Image[CharPixel] = {
-    val table = new ConvertRGBtoChar
+    val table = new ConvertRGBtoChar(new BourkeTable)
 
     val convertor = new ConvertImage[RGBPixel, CharPixel](
       (x: RGBPixel) => new CharPixel(table.ConvertToChar(x.red, x.green, x.blue))
     )
 
     x: Image[RGBPixel] => convertor.convert(x)
+  }
+
+  def CharInverter: CharPixel => CharPixel = {
+    val inverter = new Inverter(new BourkeTable)
+    (a:CharPixel) => inverter.apply(a)
   }
 }
