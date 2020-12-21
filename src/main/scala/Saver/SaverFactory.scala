55ff5fa1c752d0f7278d79786b2972699ed5fcb0
Xuan Thang Nguyen <nguyexu2@fit.cvut.cz>
2020-12-21 12:33:11 +0100

    saver factory

--- src/main/scala/Saver/SaverFactory.scala
+++ src/main/scala/Saver/SaverFactory.scala
@@ -1,7 +1,20 @@
 package Saver
 
-import Parameters.Parameter
+import Image.Image
+import Parameters.{OutputConsole, OutputLocation, OutputParam, Parameter}
 
 object SaverFactory {
-  def get(seq: Seq[Parameter])= ???
+  def get(seq: Seq[Parameter]): Option[Image[_] => Unit] = {
+    for (param <- seq) {
+      param match {
+        case _: OutputConsole => return Some((img: Image[_]) => ConsolePrint.save(img))
+        case x: OutputLocation =>
+          val fileSaver = new FileSaver(x.path)
+          return Some((img: Image[_]) => fileSaver.save(img))
+        case _: OutputParam => throw new IllegalArgumentException("output param was not implemented yet")
+        case _ =>
+      }
+    }
+    None
+  }
 }
