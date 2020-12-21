94066e7c025474b956157818aafbb4108ff6f2a0
Xuan Thang Nguyen <nguyexu2@fit.cvut.cz>
2020-12-21 13:21:13 +0100

    return Option instead of throwing with no input

--- src/main/scala/Loader/LoaderFactory.scala
+++ src/main/scala/Loader/LoaderFactory.scala
@@ -8,19 +8,19 @@ import Parameters.{InputLocation, InputParam, Parameter}
 object LoaderFactory {
   final val supportedFormats = Array("png", "jpg")
 
-  def get(args: Seq[Parameter]): FileImageLoader = {
+  def get(args: Seq[Parameter]): Option[FileImageLoader] = {
     for (param <- args) {
       param match {
         case x: InputLocation =>
           val format = x.path.substring(x.path.lastIndexOf('.') + 1)
           if (!supportedFormats.contains(format))
             throw new IllegalArgumentException(s"file of $format format not supported")
-          return new FileImageLoader(new File(x.path))
+          return Some(new FileImageLoader(new File(x.path)))
         case _: InputParam => throw new IllegalArgumentException("missing implementation for input parameter")
         case _ =>
       }
     }
 
-    throw new IllegalArgumentException("missing input")
+    None
   }
 }
