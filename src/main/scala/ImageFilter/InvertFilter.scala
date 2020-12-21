c14be5c81f695cd5eaf7a7bf28125fd0f3fe7388
Xuan Thang Nguyen <nguyexu2@fit.cvut.cz>
2020-12-21 01:57:51 +0100

    invert and tests

--- src/main/scala/ImageFilter/InvertFilter.scala
+++ src/main/scala/ImageFilter/InvertFilter.scala
@@ -6,7 +6,7 @@ class InvertFilter[A](invert: A => A) extends Filter{
 
   override def filter[T:ClassTag](arr:Array[Array[T]]) :Array[Array[T]] =
   {
-    if(!arr(0)(0).isInstanceOf[A])
+    if(!arr.isEmpty && !arr(0)(0).isInstanceOf[A])
       throw new IllegalArgumentException("type do not match")
 
     arr.map(_.map(x=>invert(x.asInstanceOf[A]).asInstanceOf[T]))
---
+++ src/test/scala/Filters/InvertFilterTest.scala
@@ -0,0 +1,23 @@
+package Filters
+
+import ImageFilter.InvertFilter
+import org.scalatest.FunSuite
+
+class InvertFilterTest extends FunSuite{
+  test("normal change") {
+    val filter = new InvertFilter( (e:Int) => if(e >= 255) 0 else 255 -e)
+    val input = Array(Array(255, 50), Array(60, 0))
+    val output = filter.filter(input)
+
+    input(0)(0) = -454054
+    assert(output.length == input.length)
+    assert(output(0).sameElements(Array(0, 255 -50)))
+    assert(output(1).sameElements(Array(255-60, 255)))
+  }
+
+  test("type mismatch") {
+    val filter = new InvertFilter( (e:Int) => e*50)
+    val input = Array(Array("a", "b"))
+    assertThrows[Throwable](filter.filter(input))
+  }
+}
