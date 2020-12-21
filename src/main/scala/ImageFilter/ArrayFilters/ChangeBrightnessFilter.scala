bd125f8c20e1cb90048d6307b565f71e4e9ff807
Xuan Thang Nguyen <nguyexu2@fit.cvut.cz>
2020-12-21 21:44:38 +0100

    brightness comments and tests

--- src/main/scala/ImageFilter/ArrayFilters/ChangeBrightnessFilter.scala
+++ src/main/scala/ImageFilter/ArrayFilters/ChangeBrightnessFilter.scala
@@ -3,9 +3,14 @@ package ImageFilter.ArrayFilters
 import scala.reflect.ClassTag
 
 class ChangeBrightnessFilter[A](f: A => A) extends Filter {
-
+  /**
+   * @param arr input that method will be apply function on
+   * @tparam T type of array AND function passed in ctor, make sure theyre the same, else an exception will be thrown
+   * @return modified array
+   */
+    @throws[IllegalArgumentException]
   override def filter[T: ClassTag](arr: Array[Array[T]]): Array[Array[T]] = {
-    if (!arr.isEmpty && !arr(0)(0).isInstanceOf[A])
+    if (!arr.isEmpty && !arr(0).isEmpty && !arr(0)(0).isInstanceOf[A])
       throw new IllegalArgumentException("type do not match")
 
     arr.map(_.map(x => {
--- src/test/scala/ImageFilter/ArrayFilters/BrightnessChangeTester.scala
+++ src/test/scala/ImageFilter/ArrayFilters/BrightnessChangeTester.scala
@@ -3,7 +3,23 @@ package ImageFilter.ArrayFilters
 import org.scalatest.FunSuite
 
 class BrightnessChangeTester extends FunSuite {
+  test("empty input"){
+    val filter = new ChangeBrightnessFilter( (e:Int) => e*50)
+    val input = Array(Array[Int]())
+    assert(filter.filter(input).length == 1)
+  }
+
   test("no brightness change") {
+    val filter = new ChangeBrightnessFilter( (e:Int) => e)
+    val input = Array(Array(0, 1), Array(5, -1))
+    val output = filter.filter(input)
+
+    assert(output.length == input.length)
+    assert(output(0).sameElements(Array(0, 1)))
+    assert(output(1).sameElements(Array(5, -1)))
+  }
+
+  test("immutability") {
     val filter = new ChangeBrightnessFilter( (e:Int) => e*50)
     val input = Array(Array(0, 1), Array(5, -1))
     val output = filter.filter(input)
