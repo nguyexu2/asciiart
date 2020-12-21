2a3edbc5003b5fe5ff3278146ebd6aa1a1724613
Xuan Thang Nguyen <nguyexu2@fit.cvut.cz>
2020-12-21 03:00:39 +0100

    implement filterList using composite pattern

--- src/main/scala/ImageFilter/FilterList.scala
+++ src/main/scala/ImageFilter/FilterList.scala
@@ -1,10 +1,13 @@
 package ImageFilter
 
-
-import java.util
-
 import scala.reflect.ClassTag
 
-class FilterList extends Filter {
-  override def filter[T:ClassTag](arr:Array[Array[T]]) :Array[Array[T]] = ???
+class FilterList(seq: Seq[Filter]) extends Filter {
+  override def filter[T: ClassTag](arr: Array[Array[T]]): Array[Array[T]] = {
+    var out = arr.map(_.clone())
+    for(filter <- seq){
+      out = filter.filter(out)
+    }
+    out
+  }
 }
