03766e45b34d1991a1a130988b19b3942d1ca5be
Xuan Thang Nguyen <nguyexu2@fit.cvut.cz>
2020-12-21 03:01:50 +0100

    basic filterfactory, missing some functions

--- src/main/scala/ImageFilter/FilterFactory.scala
+++ src/main/scala/ImageFilter/FilterFactory.scala
@@ -1,5 +1,34 @@
 package ImageFilter
 
-object FilterFactory  {
 
+import Image.CharPixel
+import ImageFilter.ArrayFilters.{ChangeBrightnessFilter, FlipXFilter, FlipYFilter, InvertFilter, RotateFilter, ScaleFilter}
+import Parameters.{Brightness, FlipX, FlipY, Invert, Parameter, Rotate, Scale}
+
+object FilterFactory {
+  def get(seq: Seq[Parameter]): Filter = {
+    var ret = List[Filter]()
+    for (param <- seq) {
+      param match {
+        case _: FlipX => ret = ret.appended(new FlipXFilter)
+        case _: FlipY => ret = ret.appended(new FlipYFilter)
+        case _: Invert => ret = ret.appended(new InvertFilter(
+          (e: CharPixel) => e
+          //TODO
+        ))
+        case x: Rotate => ret = ret.appended(new RotateFilter(x.degree))
+        case x: Scale => ret = ret.appended(new ScaleFilter(x.value,
+          (l: Seq[CharPixel]) => l.head
+          //TODO
+        ))
+        case x:Brightness => ret = ret.appended(new ChangeBrightnessFilter(
+          //TODO
+          (e:CharPixel) => e
+        ))
+        case _ =>
+      }
+
+    }
+    new FilterList(ret)
+  }
 }
