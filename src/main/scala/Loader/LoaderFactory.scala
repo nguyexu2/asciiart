1e476398d7021329da45ae2d8cd1c36cc73d708f
Xuan Thang Nguyen <nguyexu2@fit.cvut.cz>
2020-12-21 12:34:49 +0100

    check for missing implementation for inputs

--- src/main/scala/Loader/LoaderFactory.scala
+++ src/main/scala/Loader/LoaderFactory.scala
@@ -3,19 +3,20 @@ package Loader
 import java.io.File
 
 import Loader.ImageLoaders.FileImageLoader
-import Parameters.{InputLocation, Parameter}
+import Parameters.{InputLocation, InputParam, Parameter}
 
 object LoaderFactory {
   final val supportedFormats = Array("png", "jpg")
 
-  def get(args: Seq[Parameter]) : FileImageLoader = {
+  def get(args: Seq[Parameter]): FileImageLoader = {
     for (param <- args) {
       param match {
         case x: InputLocation =>
           val format = x.path.substring(x.path.lastIndexOf('.') + 1)
-          if(! supportedFormats.contains(format))
+          if (!supportedFormats.contains(format))
             throw new IllegalArgumentException(s"file of $format format not supported")
           return new FileImageLoader(new File(x.path))
+        case _: InputParam => throw new IllegalArgumentException("missing implementation for input parameter")
         case _ =>
       }
     }
